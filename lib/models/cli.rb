58e2d72f332b2ee19c7f2a58a57c75695d81c3ad
Anika Bernstein <anikajb92@gmail.com>
2022-03-21 17:17:42 -0700

    created prompt and welcome instance methods

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,5 +1,14 @@
 class Cli
-    def welcome
-        puts "WELCOME TO SHOW SELECTOR"
+
+    def prompt
+        TTY::Prompt.new
     end
+
+    def welcome
+        system('clear')
+        puts "WELCOME TO SHOW SELECTOR"
+        ask = prompt.yes?("Would you like to select some shows?")
+        binding.pry
+    end
+
 end 
