bf78822f939588b098ff2a035b31e204046a1d1f
Anika Bernstein <anikajb92@gmail.com>
2022-03-21 18:58:30 -0700

    Returning all shows after user responds to prompt

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -8,7 +8,16 @@ class Cli
         system('clear')
         puts "WELCOME TO SHOW SELECTOR"
         ask = prompt.yes?("Would you like to select some shows?")
-        binding.pry
+        if ask
+            list_shows
+        else
+            puts "Too bad"
+            welcome
+        end
+    end
+
+    def list_shows
+        puts Show.all.pluck(:title)
     end
 
 end 
